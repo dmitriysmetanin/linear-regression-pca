2ef9a107421f34780879647c990234268479d974
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-05-31 12:55:17 +0300

    улучшил лассо-модель

--- linear-regression-pca/lasso.py
+++ linear-regression-pca/lasso.py
@@ -1,19 +1,23 @@
 import pandas as pd
 from sklearn.linear_model import Lasso
+from sklearn. linear_model import LassoCV
+from sklearn. model_selection import RepeatedKFold
+from numpy import arange
 
 # Импорт файла и чтение данных
 file = pd.read_csv('files/bikes_rent.csv')
 X = file[['yr', 'season', 'temp']]
 y = file[['cnt']]
 
-def search_most_infl_feature(X, y, alpha):
-    lasso = Lasso(alpha=alpha)
-    lasso.fit(X, y)
+def search_most_infl_feature(X, y):
+    cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+    cv_model = LassoCV(alphas=arange(0, 1, 0.01), cv=cv, n_jobs=-1)
+    cv_model.fit(X,y)
 
-    index = abs(lasso.coef_).argmax()
+    index = abs(cv_model.coef_).argmax()
     max_value = X.columns[index]
 
     return (index, max_value)
 
-print(search_most_infl_feature(X, y, alpha=0.1))
+print(search_most_infl_feature(X, y))
 
